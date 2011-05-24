d50686509c895991d63be0ee3e33e9565212b154
Dako <dako@contextswitch.eu>
2011-05-24 20:10:30 +0200

    Python übernimmt das Extrahieren des Archives, bisher nur Gerüst, da Kompression noch nicht sicher.

---
+++ backend/Azuremd.Backend/bin/scripts/extract.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+def extract():
+	path = arg0 # pfad
+	name = arg1 # name
+	
+	# 7z magic ...
+	#subprocess.args(['7z', 'x', path])
+	
+if __name__ == "__main__":
+	extract()
